f224d63ffe795627c367802127a04214d8fcc3dc
divyapatil2013 <115869875+divyapatil2013@users.noreply.github.com>
2026-04-14 18:00:44 +0530

    Add configuration for Railway deployment

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn pehchan_webapp.wsgi
--- pehchan_webapp/settings.py
+++ pehchan_webapp/settings.py
@@ -146,10 +146,10 @@ LOGOUT_REDIRECT_URL = 'home'
 
 # Email Configuration
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'miniprojecttest5260@gmail.com'
-EMAIL_HOST_PASSWORD = 'wqescbdcxlnelfsc'
+EMAIL_HOST = os.environ.get('EMAIL_HOST', 'smtp.gmail.com')
+EMAIL_PORT = int(os.environ.get('EMAIL_PORT', 587))
+EMAIL_USE_TLS = os.environ.get('EMAIL_USE_TLS', 'True') == 'True'
+EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER', 'miniprojecttest5260@gmail.com')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD', 'wqescbdcxlnelfsc')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
