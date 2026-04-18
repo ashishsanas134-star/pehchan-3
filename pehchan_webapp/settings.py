2a041e124e360f2fba9b17417a195e6677ce689e
divyapatil2013 <115869875+divyapatil2013@users.noreply.github.com>
2026-04-18 23:09:03 +0530

    Fix: Move admin creation to reliable release Command

--- pehchan_webapp/settings.py
+++ pehchan_webapp/settings.py
@@ -102,7 +102,8 @@ WSGI_APPLICATION = 'pehchan_webapp.wsgi.application'
 DATABASES = {
     'default': dj_database_url.config(
         default=f'sqlite:///{BASE_DIR / "db.sqlite3"}',
-        conn_max_age=600
+        conn_max_age=0,
+        conn_health_checks=True,
     )
 }
 
